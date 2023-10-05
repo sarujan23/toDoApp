99c20c7581409947c3c2b8b3284fd89c2bd28563
sarujan23 <abisaru81@gmail.com>
2023-10-05 22:09:00 +0530

    activity feed commit

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import ToDo from './components/ToDo'
 import SideBar from './components/SideBar'
 import NavBar from './components/NavBar';
-
+import ActivityFeed from './components/ActivityFeed';
 function App() {
   return (
     <div className="App">
       <ToDo/>
       <SideBar/>
       <NavBar/>
+      <ActivityFeed/>
     </div>
   );
 }
---
+++ src/components/ActivityFeed.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import './ActivityFeed.css'
+
+function ActivityFeed() {
+  return (
+
+    <div className="activity">
+        <div className='activityheader'>
+        <div className="frame67">
+            <div className='frame65'>
+                <div className='frame64'><p id="aheader">Activity Feed</p></div>
+            </div>
+        </div>
+        </div>
+        <div className='group1732'>
+            <div className='frame1730'>
+                <div className='group1728'><img id="face1" src="/image/face1.png" alt="ellipse6"></img></div>
+                <div className='frame1729'>
+                    <p id="text1">Kushantha Charuka created Contract #00124 need John Beige’s signature</p>
+                    <p id="text2">Sep 16, 2022 at 11:30 AM</p>
+                </div>
+            </div>
+            <div className='frame1731'>
+                <div className='group1728'><img id="face2" src="/image/face2.png" alt="ellipse7"></img></div>
+                <div className='frame1729'>
+                    <p id="text1">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas pretium neque</p>
+                    <p id="text2">Sep 16, 2022 at 11:45 AM</p>
+                </div>
+            </div>
+            <div className='frame1732'>
+                <div className='group1728'><img id="face3" src="/image/face3.png" alt="ellipse7"></img></div>
+                <div className='frame1729'>
+                    <p id="text1">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas pretium neque</p>
+                    <p id="text2">Sep 16, 2022 at 11:45 AM</p>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+
+  )
+}
+
+export default ActivityFeed
