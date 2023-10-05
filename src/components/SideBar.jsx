7b30b9635158665bb5845230bc6e19b34882e297
sarujan23 <abisaru81@gmail.com>
2023-10-05 19:42:43 +0530

    welcome back commit

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -3,6 +3,7 @@ import './SideBar.css'
 
 function SideBar() {
   return (
+  <div>
     <div className='container-fluid'>
         <div class="panel panel-default">
             <div class="panel-heading"><h3 id="heading">Acmy Solutions</h3></div>
@@ -15,6 +16,21 @@ function SideBar() {
             </div>
         </div>     
     </div>
+
+    <div className="frame1743">
+            <h3 id="welcome">Welcome Back, Jhon Doe</h3>
+            <p id="welcomenote">The end of the year is coming. Are you planning your performance interviews? You can do this super efficiently with Acmy.<br/>
+                <a id="link" href="http://localhost:3000/">Look here for more information</a></p>
+            <div className="group1770">
+                <img id="round1" src="/image/round01.png" alt=""></img>
+                <img id="round2" src="/image/round02.png" alt=""></img>
+                <img id="round3" src="/image/round03.png" alt=""></img>
+                <img id="round4" src="/image/round04.png" alt=""></img>
+                <img id="round5" src="/image/round05.png" alt=""></img>
+            </div>
+            <div className="close"><img id="closeicon" src="/image/close.png" alt="close"></img></div>
+        </div>
+  </div>  
   )
 }
 
